fcdfd0300800350e4fd996cd2a15b8963915e090
Ahsan Saddique <Ahsan Saddique>
2016-08-13 00:28:34 +0500

    Write sql queries and get data from Database

--- index.php
+++ index.php
@@ -49,10 +49,9 @@
 // echo person::AVG_AGE;
 echo $detail->getFullName();
 echo Author::authorCenturyPopular();
-echo 'welcome to GOA Saddiq';
 
 //**********************************
-$dbPassword="asif211";
+$dbPassword="asdf123";
 $dbUserName="php_project";
 $dbServer="localhost";
 $dbName="php_project";
@@ -60,40 +59,48 @@ $dbName="php_project";
 $connection=new mysqli($dbServer,$dbUserName,$dbPassword,$dbName);
 if($connection->connect_errno)
 {
-    exit("Database has encoutered an error: ".$connection->connect_error);
+    exit("Database has encoutered an error:".$connection->connect_error);
 }
 
 
 //$query= "DELETE FROM php_project WHERE id = 2";
 //$query="UPDATE php_project SET penName='L.M. Mongomery' WHERE id=2 " ;
-$query="INSERT INTO php_project (firstName,lastName,penName) VALUES ('Kashif','Saddiq','dollar')";
-$resultobj=$connection->query($query);
-
+//$query="INSERT INTO php_project (id,firstName,lastName,penName) VALUES (5,'Aslam','john','mongomery')";
+//$query="SELECT firsName,lastName,PenName FROM php_project ORDOR BY firstName";
+$query='SELECT firstName,lastName FROM php_project ORDER BY lastName';
+$resultObj=$connection->query($query);
 //Select DATA from Data base
-if($resultobj->num_rows>0)
+if($resultObj->num_rows>0)
 {
-    while ($getdata=$resultobj->fetch_assoc())
+    while ($SighleRowFromQuery=$resultObj->fetch_assoc())
     {
-        echo 'Author: '.$resultobj['firstName'].PHP_EOL; ;
+        echo '--Author: '.$SighleRowFromQuery['lastName'].PHP_EOL; ;
+           //print_r($SighleRowFromQuery);
     }
-    
+   
 }
 
-
-
+//echo "newly created  author di: ".$connection->insert_id ;
+$resultObj->close();
 $connection->close();
  
   ?>
 
 
-
+ 
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="style1.css">
         <meta charset="UTF-8">
         <title>PHP websites</title>
     </head>
     <body>
-        <h1>Welcome to the New Websites</h1>
+        <h1 class="head1">Welcome to the PHP Development Websites</h1>
+        <h1>You will be familiar with the queries by this site: </h1>
+        First Name: <input type="text">
+         Last Name: <input type="text">
+          Pen Name:  <input type="text">
+           
     </body>
 </html>
